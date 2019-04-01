69eb5f5fd702fda101b691d467eed6089c6e0888
iseulde <wp@iseulde.com>
2019-04-01 16:47:53 +0200

    Memoize props to context

--- packages/block-editor/src/components/block-edit/index.js
+++ packages/block-editor/src/components/block-edit/index.js
@@ -1,17 +1,46 @@
 /**
+ * External dependencies
+ */
+import memize from 'memize';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
 import Edit from './edit';
 import { BlockEditContextProvider } from './context';
 
-function BlockEdit( props ) {
-	const { name, isSelected, clientId } = props;
+class BlockEdit extends Component {
+	constructor() {
+		super( ...arguments );
 
-	return (
-		<BlockEditContextProvider value={ { name, isSelected, clientId } }>
-			<Edit { ...props } />
-		</BlockEditContextProvider>
-	);
+		// It is important to return the same object if props haven't changed
+		// to avoid  unnecessary rerenders.
+		// See https://reactjs.org/docs/context.html#caveats.
+		this.propsToContext = memize(
+			this.propsToContext.bind( this ),
+			{ maxSize: 1 }
+		);
+	}
+
+	propsToContext( name, isSelected, clientId ) {
+		return { name, isSelected, clientId };
+	}
+
+	render() {
+		const { name, isSelected, clientId } = this.props;
+		const value = this.propsToContext( name, isSelected, clientId );
+
+		return (
+			<BlockEditContextProvider value={ value }>
+				<Edit { ...this.props } />
+			</BlockEditContextProvider>
+		);
+	}
 }
 
 export default BlockEdit;
